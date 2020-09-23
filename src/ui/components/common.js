ecda466fb8afd0949f9b3c60ea74a921e08ec065
Samus <26967164+SamusElderg@users.noreply.github.com>
2020-09-23 23:21:15 +0800

    wallet-responive
    Separated the wallet connection
    
    Will call for it in header if above 767px width
    
    Will call for it in footer if below 768px width. Sticky in bottom right for mobile

--- src/ui/components/common.js
+++ src/ui/components/common.js
@@ -11,6 +11,40 @@ import { paneStyles, colStyles } from '../components/styles'
 import { getTokenSymbol } from '../../client/web3'
 import { H1, HR, Colour, Text, Center, Label, Sublabel, LabelGroup } from '../components/elements'
 
+// Check If Responsive
+export const useWindowSize = () => {
+  // Initialize state with undefined width/height so server and client renders match
+  const [windowSize, setWindowSize] = useState({
+    width: undefined,
+    height: undefined,
+  });
+
+  useEffect(() => {
+    // Handler to call on window resize
+    function handleResize() {
+      // Set window width/height to state
+      setWindowSize({
+        width: window.innerWidth,
+        height: window.innerHeight,
+      });
+    }
+
+    // Add event listener
+    window.addEventListener("resize", handleResize);
+
+    // Call handler right away so state gets updated with initial window size
+    handleResize();
+
+    // Remove event listener on cleanup
+    return () => window.removeEventListener("resize", handleResize);
+  }, []); // Empty array ensures that effect is only run on mount
+
+  return windowSize;
+}
+
+
+
+
 export const BreadcrumbCombo = (props) => {
 
   return (
