f06297d5443253c9df81c3a8fde71e8d499f0dd0
Samus <26967164+SamusElderg@users.noreply.github.com>
2020-10-12 18:06:26 +0800

    add modal for low BNB warning

--- src/pages/TradePane.js
+++ src/pages/TradePane.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useState} from "react";
 
 import InputPane from "./InputPane";
 
@@ -8,6 +8,10 @@ import {LoadingOutlined, UnlockOutlined} from "@ant-design/icons";
 
 import {
     Button,
+    Modal,
+    ModalHeader,
+    ModalBody,
+    ModalFooter
 } from "reactstrap"
 
 import {withNamespaces} from "react-i18next";
@@ -16,6 +20,23 @@ import { withRouter } from "react-router-dom";
 
 
 export const TradePane = (props) => {
+
+    const [showModal, setShowModal] = useState(false);
+
+    const toggle = () => setShowModal(!showModal);
+
+    const checkEnoughForGas = () => {
+        if (props.tradeData.symbol === 'BNB') { // if input Symbol is BNB
+            if (convertFromWei(props.tradeData.balance - props.tradeData.input) < 0.05) {   //if (wallet BNB balance) minus (transaction BNB amount) < 0.05
+                setShowModal(true)
+            }    
+            else props.trade()
+        }
+
+        else {
+            props.trade()
+        }
+    }
 
     return (
         <>
@@ -53,19 +74,52 @@ export const TradePane = (props) => {
             </div>
             <br/><br/>
             <div className="text-center">
-                {!props.approval &&
-                <Button size="lg" color="success" onClick={props.unlock}><UnlockOutlined/>{props.t("Unlock")}</Button>
+                {!props.approval && (props.tradeData.balance > 0) &&
+                    <Button size="lg" color="success" onClick={props.unlock}><UnlockOutlined/>{props.t("Unlock")}</Button>
                 }
 
                 {props.approval && props.startTx && !props.endTx &&
-                <Button size="lg" color="success" onClick={props.trade}>
-                    <LoadingOutlined/>{`${props.type} ${props.pool.symbol}`}</Button>
+                    <Button 
+                    size="lg" 
+                    color="success" 
+                    onClick={checkEnoughForGas}
+                    >
+                        <LoadingOutlined/>{`${props.type} ${props.pool.symbol}`}
+                    </Button>
                 }
 
                 {props.approval && !props.startTx && (props.tradeData.balance > 0) &&
-                <Button
-                    size="lg" color="success" onClick={props.trade}>{`${props.type} ${props.pool.symbol}`}</Button>
+                    <Button 
+                    size="lg" 
+                    color="success" 
+                    onClick={checkEnoughForGas}
+                    >
+                        {`${props.type} ${props.pool.symbol}`}
+                    </Button>
                 }
+
+                <Modal isOpen={showModal} toggle={toggle}>
+                        <ModalHeader toggle={toggle}>BNB balance will be low after this transaction!</ModalHeader>
+                        <ModalBody>
+                            This transaction will leave you with a very low BNB balance ({convertFromWei(props.tradeData.balance - props.tradeData.input)} BNB)<br/>
+                            Please ensure you understand that BNB is used as 'gas' for the BSC network.<br/>
+                            If you do not have any/enough BNB in your wallet you may not be able to transfer assets or interact with BSC DApps after this transaction.<br/>
+                            Keep in mind however, gas fees are usually very low (~0.005 BNB).<br/>
+                            0.05 BNB is usually enough for 10+ transactions.<br/>
+                        </ModalBody>
+                        <ModalFooter>
+                            <Button 
+                            color="primary" 
+                            onClick={() => {
+                                toggle();
+                                props.trade();
+                            }}>
+                                Continue Txn Anyway!
+                            </Button>{' '}
+                            <Button color="secondary" onClick={toggle}>Cancel Transaction</Button>
+                        </ModalFooter>
+                </Modal>
+
             </div>
         </>
     )
