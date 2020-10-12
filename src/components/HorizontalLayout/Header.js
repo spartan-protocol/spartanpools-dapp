c49ef31d28b11346ea194a6bd4a66a8494f1ced1
Jael Romero <f1ntch@outlook.com>
2020-10-13 00:37:44 +0200

    FIX: Fonts not fully responsive in the cards #87

--- src/components/HorizontalLayout/Header.js
+++ src/components/HorizontalLayout/Header.js
@@ -24,15 +24,16 @@ import { withNamespaces } from 'react-i18next';
 
 //import components
 import AddressConn from '../CommonForBoth/AddressConn';
+import ThemeSwitch from "../CommonForBoth/TopbarDropdown/ThemeSwitch";
 //import ThemeSwitch from "../CommonForBoth/TopbarDropdown/ThemeSwitch";
 
 const Header = (props) => {
 
   const toggleFullscreen = () => {
     if (
-      !document.fullscreenElement &&
-      /* alternative standard method */ !document.mozFullScreenElement &&
-      !document.webkitFullscreenElement
+        !document.fullscreenElement &&
+        /* alternative standard method */ !document.mozFullScreenElement &&
+        !document.webkitFullscreenElement
     ) {
       // current working methods
       if (document.documentElement.requestFullscreen) {
@@ -41,7 +42,7 @@ const Header = (props) => {
         document.documentElement.mozRequestFullScreen();
       } else if (document.documentElement.webkitRequestFullscreen) {
         document.documentElement.webkitRequestFullscreen(
-          Element.ALLOW_KEYBOARD_INPUT
+            Element.ALLOW_KEYBOARD_INPUT
         );
       }
     } else {
@@ -56,100 +57,109 @@ const Header = (props) => {
   }
 
   return (
-    <React.Fragment>
-      <header id="page-topbar">
-        <div className="navbar-header">
-          <div className="d-flex">
-            <div className="navbar-brand-box">
-              <Link to="/" className="logo logo-dark">
+      <React.Fragment>
+        <header id="page-topbar">
+          <div className="navbar-header">
+            <div className="d-flex">
+              <div className="navbar-brand-box">
+                <Link to="/" className="logo logo-dark">
                 <span className="logo-sm">
                   <img src={logo} alt="" height="30" />
                 </span>
-                <span className="logo-lg">
+                  <span className="logo-lg">
                   <img src={logoDark} alt="" height="17" />
                 </span>
-              </Link>
+                </Link>
 
-              <Link to="/" className="logo logo-light">
+                <Link to="/" className="logo logo-light">
                 <span className="logo-sm">
                   <img src={logoLightSvg} alt="" height="40" />
                 </span>
-                <span className="logo-lg">
+                  <span className="logo-lg">
                   <img src={logoLight} alt="" height="55" />
                 </span>
-              </Link>
-            </div>
-
-          {/*
-           <ThemeSwitch/>
-          */}
-
-            <button
-              type="button"
-              className="btn btn-sm px-3 font-size-16 d-lg-none header-item waves-effect waves-light"
-              onClick={props.toggleNav}
-              >
-              <i className="fa fa-fw fa-bars"></i>
-            </button>
-          </div>
-
-          <div className="d-flex">
-            <div className="dropdown d-inline-block d-lg-none ml-2">
-            </div>
-
-            <LanguageDropdown />
-
-            <UncontrolledDropdown className="d-none d-lg-inline-block ml-1">
-              <DropdownToggle className="btn header-item noti-icon waves-effect" caret tag="button">
-                <i className="bx bx-customize"></i>
-              </DropdownToggle>
-              <DropdownMenu className="dropdown-menu-lg" right>
-                <div className="px-lg-2">
-                  <Row className="no-gutters">
-                    <Col>
-                      <a className="dropdown-icon-item" target="_blank" href="https://github.com/spartan-protocol" rel="noopener noreferrer">
-                        <img src={github} alt="Github" />
-                        <span>GitHub</span>
-                      </a>
-                    </Col>
-                    <Col>
-                      <a className="dropdown-icon-item" target="_blank" href="https://medium.com/spartanprotocol" rel="noopener noreferrer">
-                        <img src={medium} alt="Medium" />
-                        <span>Medium</span>
-                      </a>
-                    </Col>
-                    <Col>
-                      <a className="dropdown-icon-item" target="_blank" href="https://twitter.com/SpartanProtocol" rel="noopener noreferrer">
-                        <img src={twitter} alt="Twitter" />
-                        <span>Twitter</span>
-                      </a>
-                    </Col>
-                  </Row>
-                </div>
-              </DropdownMenu>
-            </UncontrolledDropdown>
+                </Link>
+              </div>
 
 
-            <div className="dropdown d-none d-lg-inline-block ml-1">
+
+
+
               <button
-                type="button"
-                className="btn header-item noti-icon waves-effect"
-                onClick={toggleFullscreen}
-                data-toggle="fullscreen"
+                  type="button"
+                  className="btn btn-sm px-3 font-size-16 d-lg-none header-item waves-effect waves-light"
+                  onClick={props.toggleNav}
               >
-                <i className="bx bx-fullscreen"></i>
+                <i className="fa fa-fw fa-bars"></i>
               </button>
+
+
+
             </div>
 
-            <NotificationDropdown />
+            <div className="d-flex">
+              <div className="dropdown d-inline-block d-lg-none ml-2">
+              </div>
 
-            <div>
+
+              <LanguageDropdown />
+
+              <ThemeSwitch/>
+
+
+              <UncontrolledDropdown className="d-none d-lg-inline-block ml-1">
+                <DropdownToggle className="btn header-item noti-icon waves-effect" caret tag="button">
+                  <i className="bx bx-customize"></i>
+                </DropdownToggle>
+                <DropdownMenu className="dropdown-menu-lg" right>
+                  <div className="px-lg-2">
+                    <Row className="no-gutters">
+                      <Col>
+                        <a className="dropdown-icon-item" target="_blank" href="https://github.com/spartan-protocol" rel="noopener noreferrer">
+                          <img src={github} alt="Github" />
+                          <span>GitHub</span>
+                        </a>
+                      </Col>
+                      <Col>
+                        <a className="dropdown-icon-item" target="_blank" href="https://medium.com/spartanprotocol" rel="noopener noreferrer">
+                          <img src={medium} alt="Medium" />
+                          <span>Medium</span>
+                        </a>
+                      </Col>
+                      <Col>
+                        <a className="dropdown-icon-item" target="_blank" href="https://twitter.com/SpartanProtocol" rel="noopener noreferrer">
+                          <img src={twitter} alt="Twitter" />
+                          <span>Twitter</span>
+                        </a>
+                      </Col>
+                    </Row>
+                  </div>
+                </DropdownMenu>
+              </UncontrolledDropdown>
+
+
+              <div className="dropdown d-none d-lg-inline-block ml-1">
+                <button
+                    type="button"
+                    className="btn header-item noti-icon waves-effect"
+                    onClick={toggleFullscreen}
+                    data-toggle="fullscreen"
+                >
+                  <i className="bx bx-fullscreen"></i>
+                </button>
+
+              </div>
+
+              <NotificationDropdown />
+
+
+              <div>
                 <AddressConn />
+              </div>
             </div>
           </div>
-        </div>
-      </header>
-    </React.Fragment>
+        </header>
+      </React.Fragment>
   );
 }
 
