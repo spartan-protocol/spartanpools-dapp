2ffb755c36dcdf6ee814c1024b5a7a4087ae543d
Jael Romero <f1ntch@outlook.com>
2020-10-20 21:07:00 +0200

    closes #137

--- src/components/Sections/PoolPaneSide.js
+++ src/components/Sections/PoolPaneSide.js
@@ -18,9 +18,12 @@ const PoolPaneSide = (props) => {
 
     const back = () => {
         props.history.push('/pools')
-    }
+    };
+
 
     return (
+
+
         <Card className="h-100">
             <CardBody>
 
@@ -47,60 +50,62 @@ const PoolPaneSide = (props) => {
                             <h4 className="strong">{formatAllUSD(props.price, 1)}</h4>
                         </div>
                     </Col>
-                </Row>
 
-                <div className="table-responsive mt-4">
-                    <table className="table table-centered table-nowrap mb-0">
-                        <tbody>
-                        <tr>
-                            <td>
-                                <p className="mb-0 text-left">{props.t("Spot Price")}</p>
-                            </td>
-                            <td>
-                                <h5 className="mb-0 text-right">{formatAllUnits(props.pool.price)} SPARTA</h5>
-                            </td>
-                        </tr>
-                        <tr>
-                            <td style={{width: "50%"}}>
-                                <p className="mb-0 text-left">{props.t("Volume")}</p>
-                            </td>
-                            <td style={{width: "50%"}}>
-                                <h5 className="mb-0 text-right">{formatAllUSD(convertFromWei(props.pool.volume), props.price)}</h5>
-                            </td>
-                        </tr>
-                        <tr>
-                            <td>
-                                <p className="mb-0 text-left">{props.t("Tx Count")}</p>
-                            </td>
-                            <td>
-                                <h5 className="mb-0 text-right">{formatAllUnits(props.pool.txCount)}</h5>
-                            </td>
-                        </tr>
-                        <tr>
-                            <td>
-                                <p className="mb-0 text-left">{props.t("Fees")}</p>
-                            </td>
-                            <td>
-                                <h5 className="mb-0 text-right">{formatAllUSD(convertFromWei(props.pool.fees), props.price)}</h5>
-                            </td>
-                        </tr>
-                        <tr>
-                            <td>
-                                <p className="mb-0 text-left">{props.t("Depth")}</p>
-                            </td>
-                            <td>
-                                <br/>
-                                <h5 className="mb-0 text-right">{formatAllUnits(convertFromWei(props.pool.tokenAmount))}</h5>
-                                <p className="mb-0 text-right">{props.pool.symbol}</p>
-                                <h5 className="mb-0 text-right">{formatAllUnits(convertFromWei(props.pool.baseAmount))}</h5>
-                                <p className="mb-0 text-right">SPARTA</p>
-                            </td>
-                        </tr>
 
-                        </tbody>
-                    </table>
-                </div>
-                <Col >
+                    <div className="table-responsive mt-4">
+                        <table className="table table-centered table-nowrap mb-0">
+                            <tbody>
+                            <tr>
+                                <td>
+                                    <p className="mb-0 text-left">{props.t("Spot Price")}</p>
+                                </td>
+                                <td>
+                                    <h5 className="mb-0 text-right">{formatAllUnits(props.pool.price)} SPARTA</h5>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td style={{width: "50%"}}>
+                                    <p className="mb-0 text-left">{props.t("Volume")}</p>
+                                </td>
+                                <td style={{width: "50%"}}>
+                                    <h5 className="mb-0 text-right">{formatAllUSD(convertFromWei(props.pool.volume), props.price)}</h5>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td>
+                                    <p className="mb-0 text-left">{props.t("Tx Count")}</p>
+                                </td>
+                                <td>
+                                    <h5 className="mb-0 text-right">{formatAllUnits(props.pool.txCount)}</h5>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td>
+                                    <p className="mb-0 text-left">{props.t("Fees")}</p>
+                                </td>
+                                <td>
+                                    <h5 className="mb-0 text-right">{formatAllUSD(convertFromWei(props.pool.fees), props.price)}</h5>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td>
+                                    <p className="mb-0 text-left">{props.t("Depth")}</p>
+                                </td>
+                                <td>
+                                    <br/>
+                                    <h5 className="mb-0 text-right">{formatAllUnits(convertFromWei(props.pool.tokenAmount))}</h5>
+                                    <p className="mb-0 text-right">{props.pool.symbol}</p>
+                                    <h5 className="mb-0 text-right">{formatAllUnits(convertFromWei(props.pool.baseAmount))}</h5>
+                                    <p className="mb-0 text-right">SPARTA</p>
+                                </td>
+                            </tr>
+
+                            </tbody>
+
+                        </table>
+
+                    </div>
+                <Col>
                     <br/>
                     <br/>
                     <br/>
@@ -109,6 +114,7 @@ const PoolPaneSide = (props) => {
                         <i className="bx bx-arrow-back font-size-12 align-middle"></i> {props.t("Back")}
                     </button>
                 </Col>
+                </Row>
             </CardBody>
         </Card>
         //           {/* <h4>APY</h4>
