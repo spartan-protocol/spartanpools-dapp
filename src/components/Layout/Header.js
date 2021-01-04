63af4bfccbfd9fc27d1ca1d8f414297506d2b34b
Samus <26967164+SamusElderg@users.noreply.github.com>
2021-01-04 23:21:14 +0800

    SPARTA to header & language dropdown
    Add SPARTA price to the header
    
    With dropdown box containing:
    
    Market cap
    Current supply
    Supply cap
    Links to smart contracts
    Also:
    Adjust the language dropdown
    
    Change 'US flag' icon to generic 'world' icon
    Fix margin between flags and language selections
    
    closes #383

--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -1,6 +1,7 @@
-import React from "react";
+import React, {useContext, useState, useEffect} from "react";
 
 import { Link } from "react-router-dom";
+import {Context} from "../../context";
 
 // Import menuDropdown
 import LanguageDropdown from "../Common/LanguageDropdown";
@@ -11,13 +12,33 @@ import logoLightSvg from "../../assets/images/icon-light.png";
 //i18n
 import { withNamespaces } from 'react-i18next';
 
-import { Row, Col, Container } from 'reactstrap';
+import { Row, Col, Container, UncontrolledDropdown, DropdownToggle, DropdownMenu, DropdownItem } from 'reactstrap';
 
 //import components
 import AddressConn from '../Common/AddressConn';
 import ThemeSwitch from "../Common/ThemeSwitch";
+import { convertFromWei, formatAllUnits, formatGranularUnits, bn } from "../../utils";
+import { getSpartaContract, SPARTA_ADDR, UTILS_ADDR, DAO_ADDR, ROUTER_ADDR, BONDv2_ADDR, BONDv3_ADDR } from "../../client/web3";
 
 const Header = (props) => {
+
+  const context = useContext(Context)
+
+  const pause = (ms) => new Promise(resolve => setTimeout(resolve, ms))
+
+  useEffect(() => {
+    getTotalSupply()
+    // eslint-disable-next-line
+  }, [])
+
+  const [totalSupply, setTotalSupply] = useState('')
+  const getTotalSupply = async () => {
+    const contract = getSpartaContract()
+    let supply = await contract.methods.totalSupply().call()
+    setTotalSupply(supply)
+    await pause(5000)
+    getTotalSupply()
+  }
 
   return (
       <React.Fragment>
@@ -28,10 +49,10 @@ const Header = (props) => {
               <Row className="align-items-center">
 
                 <Col xs={6} className="px-1 text-left">
-                  <Link to="/" className="logo d-block d-sm-none float-left mx-1">
+                  <Link to="/" className="logo d-block d-md-none float-left mx-1">
                     <img src={logoLightSvg} alt="" height="40" />
                   </Link>
-                  <Link to="/" className="logo d-none d-sm-block float-left mx-1">
+                  <Link to="/" className="logo d-none d-md-block float-left mx-1">
                     <img src={logoLight} alt="" height="55" />
                   </Link>
                   <button
@@ -45,6 +66,32 @@ const Header = (props) => {
                 </Col>
 
                 <Col xs={6} className="px-1 text-right">
+                  {context.spartanPrice &&
+                    <UncontrolledDropdown className='d-none d-sm-inline mr-3 h-100'>
+                      <DropdownToggle className='bg-light border-0' caret>
+                        <img src='./logo192.png' alt="" height='28px' /><h5 className='d-inline block ml-2 align-middle'>${formatGranularUnits(context.spartanPrice)}</h5>
+                      </DropdownToggle>
+                      <DropdownMenu className='mt-3'>
+                        <DropdownItem disabled><i className='bx bx-coin text-success mr-1' />Total Supply: {formatAllUnits(convertFromWei(totalSupply))}</DropdownItem>
+                        <DropdownItem disabled><i className='bx bx-coin-stack text-success mr-1' />Supply Cap: 300,000,000</DropdownItem>
+                        <DropdownItem disabled><i className='bx bxs-badge-dollar text-success mr-1' />Market Cap: ${formatAllUnits(convertFromWei(bn(totalSupply).times(context.spartanPrice)))} </DropdownItem>
+                        <DropdownItem divider />
+                        <DropdownItem header><div className='text-center'><i className='bx bx-edit text-success mr-1' />Contracts</div></DropdownItem>
+                        <DropdownItem>
+                          <div className='w-50 d-inline-block text-center bg-light mr-1'><a href={'https://bscscan.com/address/' + SPARTA_ADDR + '#readContract'} target='blank' className='text-dark'>SPARTA</a></div>
+                          <div className='w-50 d-inline-block text-center bg-light ml-1'><a href={'https://bscscan.com/address/' + UTILS_ADDR + '#readContract'} target='blank' className='text-dark'>UTILS</a></div>
+                        </DropdownItem>
+                        <DropdownItem>
+                          <div className='w-50 d-inline-block text-center bg-light mr-1'><a href={'https://bscscan.com/address/' + DAO_ADDR + '#readContract'} target='blank' className='text-dark'>DAO</a></div>
+                          <div className='w-50 d-inline-block text-center bg-light ml-1'><a href={'https://bscscan.com/address/' + ROUTER_ADDR + '#readContract'} target='blank' className='text-dark'>ROUTER</a></div>
+                        </DropdownItem>
+                        <DropdownItem>
+                          <div className='w-50 d-inline-block text-center bg-light mr-1'><a href={'https://bscscan.com/address/' + BONDv2_ADDR + '#readContract'} target='blank' className='text-dark'>BONDv2</a></div>
+                          <div className='w-50 d-inline-block text-center bg-light ml-1'><a href={'https://bscscan.com/address/' + BONDv3_ADDR + '#readContract'} target='blank' className='text-dark'>BONDv3</a></div>
+                        </DropdownItem>
+                      </DropdownMenu>
+                    </UncontrolledDropdown>
+                  }
                   <LanguageDropdown/>
                   <AddressConn 
                     changeStates={props.changeStates}
