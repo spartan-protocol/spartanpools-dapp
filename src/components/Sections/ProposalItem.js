cf09bd1257fc37ba8480f98b4a14a0bd3108978e
Samus <26967164+SamusElderg@users.noreply.github.com>
2021-01-16 08:28:16 +0800

    add symbol to proposalItem
    added symbol to proposalItem
    changed some wording
    changed some text formatting
    
    closes #411

--- src/components/Sections/ProposalItem.js
+++ src/components/Sections/ProposalItem.js
@@ -1,4 +1,5 @@
-import React from "react";
+import React, {useContext} from "react";
+import {Context} from "../../context";
 
 import {convertFromWei, formatAllUnits, getAddressShort, bn} from '../../utils'
 
@@ -11,6 +12,9 @@ import {withRouter} from "react-router-dom";
 import { explorerURL } from "../../client/web3";
 
 export const ProposalItem = (props) => {
+
+    const context = useContext(Context);
+
     const getStatus = () => {
         let status = 'Pending'
         if (props.finalised === true) {status = 'Finalised'}
@@ -35,11 +39,20 @@ export const ProposalItem = (props) => {
         return date
     }
 
+    const getSymbol = () => {
+        let symbol = ''
+        if (context.poolsData) {
+            symbol = context.poolsData.filter(i => i.address === props.proposedAddress)
+            if (symbol[0]) {symbol = symbol[0].symbol}
+        }
+        return symbol
+    }
+
     return (
         <>
             <Col xs='12' md='6' className='d-flex align-items-stretch px-1 px-md-2'>
                 <Card className='w-100'>
-                    <CardTitle className='pt-3 mb-3'>#{props.id} - {props.type}</CardTitle>
+                    <CardTitle className='pt-3 mb-3'>{props.type} {(props.type === 'LIST' || props.type === 'DELIST') && 'BOND: ' + getSymbol()} [ID: {props.id}]</CardTitle>
                     <CardSubtitle className='m-2'>
                         {props.type === 'START_EMISSIONS' && <h6>Turn on the SPARTA emissions</h6>}
                         {props.type === 'STOP_EMISSIONS' && <h6>Turn off the SPARTA emissions</h6>}
@@ -47,8 +60,8 @@ export const ProposalItem = (props) => {
                         {(props.type === 'DAO' || props.type === 'ROUTER' || props.type === 'UTILS' || props.type === 'INCENTIVE') && 
                             <h6>Change the {props.type} address to: <a href={explorerURL + 'address/' + props.proposedAddress} target='blank'>{getAddressShort(props.proposedAddress)}</a></h6>
                         }
-                        {props.type === 'LIST' && <h6>List BOND asset: <a href={explorerURL + 'address/' + props.proposedAddress} target='blank'>{getAddressShort(props.proposedAddress)}</a></h6>}
-                        {props.type === 'DELIST' && <h6>De-list BOND asset: <a href={explorerURL + 'address/' + props.proposedAddress} target='blank'>{getAddressShort(props.proposedAddress)}</a></h6>}
+                        {props.type === 'LIST' && <h6>List {getSymbol()} as a bond asset: <a href={explorerURL + 'address/' + props.proposedAddress} target='blank'>{getAddressShort(props.proposedAddress)}</a></h6>}
+                        {props.type === 'DELIST' && <h6>De-list {getSymbol()} as a bond asset: <a href={explorerURL + 'address/' + props.proposedAddress} target='blank'>{getAddressShort(props.proposedAddress)}</a></h6>}
                         {props.type === 'ADD_CURATED_POOL' && <h6>Add pool to curated list: <a href={explorerURL + 'address/' + props.proposedAddress} target='blank'>{getAddressShort(props.proposedAddress)}</a></h6>}
                         {props.type === 'REMOVE_CURATED_POOL' && <h6>Remove pool from curated list: <a href={explorerURL + 'address/' + props.proposedAddress} target='blank'>{getAddressShort(props.proposedAddress)}</a></h6>}
                         {props.type === 'CHALLENGE_CURATED_POOL' && <h6>Challenge low-depth curated pool: <a href={explorerURL + 'address/' + props.proposedAddress} target='blank'>{getAddressShort(props.proposedAddress)}</a></h6>}
@@ -61,16 +74,16 @@ export const ProposalItem = (props) => {
                     <CardBody className='pt-2'>
                         <Row>
                             <Col xs='6' className='d-flex align-items-stretch'>
-                                <div className='w-100 m-1 p-1 bg-light rounded'>VOTES:<br/>{formatAllUnits(bn(props.votes).div(bn(props.wholeDAOWeight)).times(100))} %</div>
+                                <div className='w-100 m-1 p-1 bg-light rounded'>Votes:<br/>{formatAllUnits(bn(props.votes).div(bn(props.wholeDAOWeight)).times(100))} %</div>
                             </Col>
                             <Col xs='6' className='d-flex align-items-stretch'>
-                                <div className='w-100 m-1 p-1 bg-light rounded'>DATE:<br/>{getDate()}</div>
+                                <div className='w-100 m-1 p-1 bg-light rounded'>Finalisation:<br/>{getDate()}</div>
                             </Col>
                             <Col xs='6' className='d-flex align-items-stretch'>
-                                <div className='w-100 m-1 p-1 bg-light rounded'>STATUS:<br/>{getStatus()}</div>
+                                <div className='w-100 m-1 p-1 bg-light rounded'>Status:<br/>{getStatus()}</div>
                             </Col>
                             <Col xs='6' className='d-flex align-items-stretch'>
-                                <div className='w-100 m-1 p-1 bg-light rounded'>WEIGHT:<br/>{getWeight()}</div>
+                                <div className='w-100 m-1 p-1 bg-light rounded'>Weight:<br/>{getWeight()}</div>
                             </Col>
                         </Row>
                     </CardBody>
