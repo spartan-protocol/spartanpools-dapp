08bddc8d0fee80f2cbc1c45902d657d754ebe4ec
Samus <26967164+SamusElderg@users.noreply.github.com>
2021-02-24 12:12:59 +0800

    fix TVL UI
    closes #478

--- src/pages/Pools.js
+++ src/pages/Pools.js
@@ -2,7 +2,7 @@ import React, {useEffect, useState, useContext} from 'react'
 import {Context} from "../context";
 import {withRouter} from "react-router-dom";
 
-import {getGlobalData} from '../client/web3'
+// import {getGlobalData} from '../client/web3'
 import {bn} from "../utils";
 
 import Breadcrumbs from "../components/Common/Breadcrumb";
@@ -16,16 +16,17 @@ const Pools = (props) => {
 
     const context = useContext(Context)
 
-    const [globalData, setGlobalData] = useState({
-        totalPooled: 0,
-        totalFees: 0,
-        totalVolume: 0,
-        addLiquidityTx: 0,
-        removeLiquidityTx: 0,
-        swapTx: 0,
-    });
+    // const [globalData, setGlobalData] = useState({
+    //     totalPooled: 0,
+    //     totalFees: 0,
+    //     totalVolume: 0,
+    //     addLiquidityTx: 0,
+    //     removeLiquidityTx: 0,
+    //     swapTx: 0,
+    // });
 
     const [totalVolume, setTotalVolume] = useState(0)
+    const [totalValueLocked, setTotalValueLocked] = useState(0)
 
     useEffect(() => {
         getData()
@@ -33,14 +34,17 @@ const Pools = (props) => {
     }, [context.poolsDataComplete])
 
     const getData = async () => {
-        setGlobalData(await getGlobalData())
+        //setGlobalData(await getGlobalData())
         if (context.poolsDataComplete) {
-            let data = context.poolsData.map(a => a.volume)
+            let data = context.poolsData.map(a => a)
             let volume = 0
+            let tvl = 0
             for (let i = 0; i < data.length; i++) {
-                volume = bn(volume).plus(bn(data[i]))
+                volume = bn(volume).plus(bn(data[i].volume))
+                tvl = bn(tvl).plus(bn(data[i].baseAmount))
             }
             setTotalVolume(volume)
+            setTotalValueLocked(tvl)
         }
     }
 
@@ -51,7 +55,7 @@ const Pools = (props) => {
                     <Breadcrumbs title={props.t("App")} breadcrumbItem={props.t("Pools")}/>
                     <Row>
                         <Col xs="12">
-                            <PoolsPaneSide globalData={globalData} totalVolume={totalVolume} />
+                            <PoolsPaneSide totalVolume={totalVolume} tvl={totalValueLocked} />
                         </Col>
                         <Col xs="12">
                             <PoolTable/>
